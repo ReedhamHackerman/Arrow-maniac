7a81568766201ab5cf70c8c4e74fa8de42ddc4f8
vaderaparth22 <vaderaparth22@gmail.com>
2021-03-09 13:02:34 -0500

    Work in progress
    Player movement work in progress

---
+++ Assets/Resources/Scripts/MoveDemo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class MoveDemo : MonoBehaviour
+{
+    [SerializeField] private int playerId;
+    [SerializeField] private float speed;
+
+    private Player player;
+    private Rigidbody2D _rb;
+    private LayerMask groundLayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        player = ReInput.players.GetPlayer(0);
+        groundLayerMask = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetButtonDown("Jump"))
+        {
+            Debug.Log("jumping...");
+        }
+
+        isGrounded();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = player.GetAxis("Move Horizontal");
+        float gravityCalculate = Physics2D.gravity.y;
+
+        Vector2 moveVector = new Vector2(horizontal, 0);
+
+        _rb.velocity = moveVector * speed * Time.fixedDeltaTime;
+    }
+
+    private void isGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -Vector2.up, groundLayerMask);
+
+        if (hitInfo)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.yellow);
+        }
+    }
+}
