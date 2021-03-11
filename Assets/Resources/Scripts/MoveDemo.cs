e866403a995d23f7e6ab7cfb41f9d34b3ba83cca
vaderaparth22 <vaderaparth22@gmail.com>
2021-03-11 14:44:40 -0500

    Player movement with Rewired completed

--- Assets/Resources/Scripts/MoveDemo.cs
+++ Assets/Resources/Scripts/MoveDemo.cs
@@ -7,47 +7,59 @@ public class MoveDemo : MonoBehaviour
 {
     [SerializeField] private int playerId;
     [SerializeField] private float speed;
+    [SerializeField] private float gravityValue;
+    [SerializeField] private float groundDetectionRange;
+    [SerializeField] private float jumpForce;
+
+    public Vector2 moveVector;
 
     private Player player;
     private Rigidbody2D _rb;
     private LayerMask groundLayerMask;
 
+    private float gravityCalculate;
+    private float horizontal;
+    public bool canJump;
+
+    public bool Grounded { get; set; } = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = ReInput.players.GetPlayer(playerId);
         _rb = GetComponent<Rigidbody2D>();
-        player = ReInput.players.GetPlayer(0);
         groundLayerMask = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.GetButtonDown("Jump"))
-        {
-            Debug.Log("jumping...");
-        }
+        Grounded = isGrounded();
+        horizontal = player.GetAxis("Move Horizontal");
 
-        isGrounded();
+        Jumping();
     }
 
     private void FixedUpdate()
     {
-        float horizontal = player.GetAxis("Move Horizontal");
-        float gravityCalculate = Physics2D.gravity.y;
-
-        Vector2 moveVector = new Vector2(horizontal, 0);
-
-        _rb.velocity = moveVector * speed * Time.fixedDeltaTime;
+        Movement();
     }
 
-    private void isGrounded()
+    private void Movement()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -Vector2.up, groundLayerMask);
+        _rb.velocity = new Vector2(horizontal * speed, _rb.velocity.y);
+    }
 
-        if (hitInfo)
-        {
-            Debug.DrawLine(transform.position, hitInfo.point, Color.yellow);
-        }
+    private void Jumping()
+    {
+        if (player.GetButtonDown("Jump") && Grounded)
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -Vector2.up, groundDetectionRange, groundLayerMask);
+
+        return hitInfo ? true : false;
     }
 }
