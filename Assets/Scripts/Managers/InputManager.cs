1d55ece5a15e1d80b4169c611f02c1d85625432b
vaderaparth22 <vaderaparth22@gmail.com>
2021-03-13 16:02:06 -0500

    Minor changes in assigning input to players on start

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,6 +5,7 @@ public class InputManager
     private Player player;
 
     public float HorizontalInput => player?.GetAxis("Move Horizontal") ?? 0f;
+    public float VerticalInput => player?.GetAxis("Move Vertical") ?? 0f;
     public bool GetJumpButtonDown => player?.GetButtonDown("Jump") ?? false;
     public bool GetDashButtonDown => player?.GetButtonDown("Dash") ?? false;
 
