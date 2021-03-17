8b67ec1d72531a0706a14001f3ccb457b76914bd
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-03-17 02:32:13 -0400

    Invisibility demo Working
    - Invisibility Ability working as intended It's mapped to Y button on controller
    - Added dissolve material to each part of the player1 prefab
    - still need to intergrade with Input Manager

--- Assets/Scripts/Abilities/Invisible.cs
+++ Assets/Scripts/Abilities/Invisible.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class Invisible : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Material disolveMat;
+    [SerializeField] private float startTime;
+    [SerializeField] private float invisibleTime = 2f;
+    [SerializeField] private float endTime;
+    [SerializeField] private bool canUseAbility = true;
+    private float fade = 1f;
+
+   
     void Start()
     {
+        disolveMat = gameObject.GetComponent<SpriteRenderer>().material;
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+
+      
+        if (Input.GetButtonDown("Jump") && canUseAbility)
+        {
+            canUseAbility = false;
+            StartCoroutine(InvisibleAbility());
+            
+        }     
+    }
+
+
+    IEnumerator InvisibleAbility()
+    {
+        while(fade> 0)
+        {
+            fade -= Time.deltaTime;
+            disolveMat.SetFloat("_Fade", fade);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(invisibleTime);
         
+
+        while (fade < 1)
+        {
+            fade += Time.deltaTime;
+            disolveMat.SetFloat("_Fade", fade);
+            yield return null;
+        }
+
     }
 }
