9d48cf83af9b3d7a81bad4ca4e5ca650af04f263
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-03-18 19:47:26 -0400

    Map Particle Stop
    - Placeholder  R and T button to stop and resume all map particle

--- Assets/Scripts/Abilities/TimeStop.cs
+++ Assets/Scripts/Abilities/TimeStop.cs
@@ -2,17 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeStop : MonoBehaviour
+public class TimeStop : Abilities
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mapParticles;
+    private List<ParticleSystem> worldParticles = new List<ParticleSystem>();
+    private ParticleSystem particle;
+
+
+    protected override void Initialize()
     {
-        
+        //worldParticles.AddRange(mapParticles.GetComponentsInChildren<ParticleSystem>());
+
+        worldParticles.AddRange(FindObjectsOfType<ParticleSystem>());
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Refresh()
     {
+
         
+        if (Input.GetKey(KeyCode.R))
+        {
+            StopWorldParticles();
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            ResumeWorldParticles();
+        }
+
+
     }
+
+    public void StopWorldParticles()
+    {
+        foreach (ParticleSystem particleSystem in worldParticles)
+        {
+            particleSystem.Pause();
+        }
+    }
+
+    public void ResumeWorldParticles()
+    {
+        foreach (ParticleSystem particleSystem in worldParticles)
+        {
+            particleSystem.Play();
+        }
+    }
+
+    
 }
