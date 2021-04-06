e2aa0afa01637dfe9a189f926adbfafc7b257dc1
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-04-05 23:09:04 -0400

    Setting Default value

--- Assets/Scripts/Player/PlayerUnit.cs
+++ Assets/Scripts/Player/PlayerUnit.cs
@@ -143,7 +143,7 @@ public class PlayerUnit : MonoBehaviour, IFreezable
 
         for (int i = 0; i < START_ARROW_COUNT; i++)
         {
-            arrowStack.Push(ArrowType.RICOCHET);
+            arrowStack.Push(ArrowType.NORMAL);
             AddNewArrowTOHUD(ArrowType.NORMAL);
         }
 
